fc7a379f853d9f4f55476573da9a5eb1c4d0d542
Agnes Sliczniak <aga@Agness-MacBook.sliczniak.net>
2016-05-06 21:40:29 -0500

    it works a littlee

--- games/js/letters/letters.js
+++ games/js/letters/letters.js
@@ -2,6 +2,7 @@
 
 // initialization
 
+	screen = {}
 	seed=""
 	
 // must be Upper case
@@ -10,15 +11,18 @@
     
     nkey2="O"
     skey2="L"
-	cl="" # clear
+	cl="" // clear
 	hi=""
 	lo=""
 	vb=""
 
 	colors=""
 
-	width = 40
-	height = 20
+	width = 0
+	height = 0
+
+	//cols = w
+	//lines = h
 
 
 // cl clear screen
@@ -27,15 +31,81 @@
 
 // call end() on game-over
 
-function init() {
-	t = "";
-	for (j = 0; j < h; j++) {
-		for (i = 0; i < w; i++)
-			t += "."
-		t += "\n"
+// draws the screen with players and enemies.
+function draw(  i, j, gameover) {
+
+	t=""
+
+	//printf("%s", carr[1] cl)
+	for (j = 1; j <= height; j = j + 1) {
+		for (i = 1; i <= width; i = i + 1) {
+			if (i == 1) {
+				if (player1h == j) {
+					//printf("%s", carr[1] "<" carr[1])
+					t+="<"
+
+					if (screen[i,j] == "X") gameover = 1
+					
+					continue
+				}
+                
+				if (player2h == j) {
+					//printf("%s", carr[1] ">" carr[1])
+					t+=">"
+
+					if (screen[i,j] == "X") gameover = 1
+					
+					continue
+				}
+			}
+			
+			t+= screen[[i,j]]
+		}
+		
+		t+= "\n"
 	}
+
+	t+= "\n" + score
 	s.innerHTML = t;
+	
+	if (gameover == 1) end()
 }
+
+
+function init(  i, j) {
+	errstat=0
+	
+	//print carr[1]
+	
+	// use terminal size if you can
+	//print cols, lines > "/dev/stderr"
+	if (cols > 0) width = cols - 1
+	if (lines > 0) height = lines - 2
+	
+	// seems to help the early distribution on some old awks
+	for (i=0; i<256; i+=1) Math.random();
+	
+	//colors=split(colors, carr)
+	
+	//print carr[1]
+
+	score=0
+
+	player1h = Math.floor(height / 3)
+    
+    player2h = 2 * Math.floor(height / 3)
+	
+	for (j = 1; j <= height; j = j + 1) {
+		for (i = 1; i <= width; i = i + 1) {
+			screen[[i,j]] = " "
+		}
+	}
+	
+	screen[[width,player1h]] = "X"
+	//printf("%s", hi)
+	draw()
+}
+
 
 function handle(c) {
 	t = c + "\n"
